51fdb2ea177a9ce9e525b0a446252a106c27b4af
Roi262 <roi262@gmail.com>
2019-12-05 16:06:30 +0200

    cleaned code

--- ex2.py
+++ ex2.py
@@ -1,25 +1,27 @@
 import nltk
-import pandas as pd
+# import pandas as pd
 # nltk.download('brown')
 from nltk.corpus import brown
 from collections import defaultdict, Counter
 import operator
-import probabilities
+from probabilities import Probabilities
 
 START, STOP = "START", "STOP"
 DYNAMIC_STOP = "*"
 
 ################# QUESTION b##################################
 
-def generate_dictionary(self, sentences):
+
+def generate_dictionary(sentences):
     """Generate dictionary: {word_type: {POS:counter}}
     sentences is a list of sentences, each sentence is list of (word, POS)
     tuples."""
     word_tags = defaultdict(Counter)
     for sentence in sentences:
         for word, pos in sentence:
-            word_tags[word][pos] +=1
+            word_tags[word][pos] += 1
     return word_tags
+
 
 def get_mle_train(words_dict):
     max_pos_train = {}
@@ -36,7 +38,8 @@ def add_unknowns(test_set, max_pos_train):
             max_pos_train[word] = 'NN'
     return max_pos_train
 
-def get_accuracy(test_set,max_pos_train):
+
+def get_accuracy(test_set, max_pos_train):
     true_positive = 0
     total_num_of_words = 0
     for sentence in test_set:
@@ -48,10 +51,11 @@ def get_accuracy(test_set,max_pos_train):
 
 
 def calc_test_error(test_set, max_pos_train):
-    """Clculate 1-accuracy"""
-    max_pos_train = add_unknowns(test_set,max_pos_train)
-    err_rate = 1 - get_accuracy(test_set,max_pos_train)
+    """Calculate 1-accuracy"""
+    max_pos_train = add_unknowns(test_set, max_pos_train)
+    err_rate = 1 - get_accuracy(test_set, max_pos_train)
     return err_rate
+
 
 def Qb(train_news, test_news):
     train_dict = generate_dictionary(train_news)
@@ -85,13 +89,14 @@ def create_viterbi_table(x, probs):
     pi tuples: (value, index)
     """
     pi = []
-    pi.append([(1,0)]*probs.S_len)
+    pi.append([(1, 0)]*probs.S_len)
     for k in range(1, len(x)):
         pi.append([(0, 0)]*probs.S_len)
         # print('k=' + str(k))
         for j in range(probs.S_len):
             # print("J = " + str(j))
-            max_index = 0 #TODO yesterday it was inited to NONE and gave errors..
+            # TODO yesterday it was inited to NONE and gave errors..
+            max_index = 0
             max_value = 0
             e = probs.e(x[k], probs.S_list[j])
             for i in range(probs.S_len):
@@ -100,12 +105,24 @@ def create_viterbi_table(x, probs):
                 cur = pi[k-1][i][0] * q * e
                 if cur > max_value:
                     max_value = cur
-                    max_index = i #means i is the best tag!
+                    max_index = i  # means i is the best tag!
             pi[k][j] = (max_value, max_index)
     return pi
 
-def viterbi(x,S,train_set):
-    probs = probabilities.Probabilities(S, train_set)
+
+def viterbi(x, S, train_set):
+    """ A function that runs the Viterbi algorithm.
+    
+    Arguments:
+        x {[List]} -- a list containing all the sentences in the test set.
+        S {[Set]} -- a set of all the tags that exist in the training set.
+        train_set {[list of lists of tuples]} -- the training set viterbi learns
+    
+    Returns:
+        [List] -- a vector (python list) of the POS tags that are the prediction of 
+        the viterbi algorithm for the sentence x.
+    """
+    probs = Probabilities(S, train_set)
     # print("viterbi")
     pi = create_viterbi_table(x, probs)
     # print(pi)
@@ -113,8 +130,8 @@ def viterbi(x,S,train_set):
     max_prob = 0
     best_index = 0
     k = -1
-    for i in range(len(pi[k])): #starts at the -1 row!
-        prob, previous_ind = pi[k][i] #finds best probabillity there
+    for i in range(len(pi[k])):  # starts at the -1 row!
+        prob, previous_ind = pi[k][i]  # finds best probabillity there
         print(pi[k][i])
         if prob > max_prob:
             best_tuple = pi[k][i]
@@ -127,14 +144,12 @@ def viterbi(x,S,train_set):
     previous_ind = pi[k][best_index][1]
     k -= 1
     while -k < len(x):
-        # print("while")
         tag_vec.append(probs.S_list[previous_ind])
         previous_ind = pi[k][previous_ind][1]
         k -= 1
 
     tag_vec = tag_vec.reverse()
     return tag_vec
-
 
 
 def get_all_tags(sentences):
@@ -145,26 +160,32 @@ def get_all_tags(sentences):
             word_tags[word].add(pos)
     return word_tags
 
+
 def initialize_S(train_set):
-#     add from test set with unknown tag as well??
+    """ Initialize the set of all tags in the training set.
+    Arguments:
+        train_set {[type]} -- the training set.
+    """
+#   TODO  add from test set with unknown tag as well??
     S = set()
     for sentence in train_set:
-        for word, pos in sentence:
-            S.add(pos)
+        for tup in sentence:
+            S.add(tup[1])
     return S
 
 
 def Qc(train_set, test_set):
-    S = initialize_S(train_set)     #Rois version
-    viterbi_res = []
+    S = initialize_S(train_set)  # Rois version
+    viterbi_results = []
     for sentence in test_set:
         x = [t[0] for t in sentence]
         viterbi_tags = viterbi(x, S, train_set)
         print("TAGS")
         print(viterbi_tags)
-        viterbi_res.append(viterbi_tags)
+        viterbi_results.append(viterbi_tags)
 
 ###################################################################
+
 
 def main():
     tagged_news = (brown.tagged_sents(categories='news'))
@@ -173,8 +194,8 @@ def main():
     test_news = tagged_news[-threshold:]
     # a = get_all_tags(train_news)
     # Qb(train_news, test_news)
-    # Qc(train_set=train_news, test_set=test_news)
-    Qc(train_news,test_news)
+    Qc(train_set=train_news, test_set=test_news)
+    Qc(train_news, test_news)
 
 
 if __name__ == '__main__':
--- probabilities.py
+++ probabilities.py
@@ -2,7 +2,11 @@ from collections import defaultdict, Counter
 
 START = "START"
 
+
 class Probabilities():
+    """ A class that contains and manages emission and transition 
+    probability dictionaries.
+    """
 
     def __init__(self, S, train_set):
         self.trans_table = []
@@ -10,19 +14,23 @@ class Probabilities():
         self.S_list = list(S)
         self.S_len = len(self.S_list)
 
-        ### emission dictionaries###
+        ### Emission dictionaries###
+
         # d1 = {(word,pos): count}
         self.d1 = defaultdict(int)
+
         # d2 = {pos : count}
         self.d2 = defaultdict(int)
 
         ### Transition dictionaries###
+
         # d3 = {(pos,prev_pos): count}
         self.d3 = defaultdict(int)
-        # d4 = {pos: count}
-        self.d4= defaultdict(int)
 
-        self.POS_dic = self.generate_pos_dic(train_set)
+        # d4 = {pos: count}
+        self.d4 = defaultdict(int)
+
+        self.generate_pos_dic(train_set)
 
     def generate_pos_dic(self, train_set):
         """Fills in dictionaries 1-4 (i.e., the emission and tranition dictionaries)"""
@@ -30,7 +38,7 @@ class Probabilities():
             for i in range(len(xy_tups)):
                 word = xy_tups[i][0]
                 pos = xy_tups[i][1]
-                self.d1[(word,pos)] +=1
+                self.d1[(word, pos)] += 1
                 self.d2[pos] += 1
                 if i != 1:
                     prev_pos = xy_tups[i-1][1]
@@ -54,59 +62,3 @@ class Probabilities():
         if self.d3[(y1, y2)] == 0:
             return 0
         return self.d3[(y1, y2)]/self.d4[y1]
-
-
-    # def emission_prob(self, x, y, set, add_one=False):
-    #     """
-    #     :param word:
-    #     :param tag:
-    #     :return: the emission probability
-    #     """
-    #     numerator = 0
-    #     denom = 0
-    #     for sentence in set:
-    #         for word, tag in sentence:
-    #             if tag == y:
-    #                 if word == x:
-    #                     numerator += 1
-    #                 denom += 1
-    #     # if add_one: TODO
-    #     # return float((numerator + 1)/(denom + get_delta()))
-    #     return float(numerator/denom)
-
-
-    # def trans_prob(self, cur_tag, prev_tag, sentence_set):
-    #     """
-    #     run over all pairs of 2 consecutive tags in corpus
-    #     :param cur_tag:
-    #     :param prev_tag:
-    #     :return:
-    #     """
-    #     numerator = 0
-    #     denom = 0
-    #     prev = START
-    #     for sentence in sentence_set:
-    #         for word, tag in sentence:
-    #             if word == START:
-    #                 continue
-    #             if tag == cur_tag:
-    #                 if prev_tag == prev:
-    #                     numerator += 1
-    #                 denom += 1
-    #             prev_tag = tag
-    #     return float(numerator/denom)
-
-    #
-    # def generate_transition_table(self, train_set):
-    #     for i in range(self.S_len):
-    #         self.trans_table.append([0]*self.S_len)
-    #         for j in range(self.S_len):
-    #             self.trans_table[i][j] = self.trans_prob(self.S_list[i], self.S_list[j], train_set)
-    #     print(self.trans_table)
-    #
-    # def generate_emission_table(self, x, train_set):
-    #     for i in range(len(x)):
-    #         self.emission_table.append([0]*self.S_len)
-    #         for j in range(self.S_len):
-    #             self.emission_table[i][j] = self.emission_prob(x[i], self.S_list[j], train_set)
-    #     print(self.emission_table)
